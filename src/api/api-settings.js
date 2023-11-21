5bbfd5f5f5ecbe795f46644789c7d563660e9773
bill.lin <bill.lin@staff.nueip.com>
2023-11-21 18:01:44 +0800

    a

--- src/api/api-settings.js
+++ src/api/api-settings.js
@@ -1,7 +1,11 @@
 export const isProduction = ["production"].includes(process.env.NODE_ENV);
 
 // APIs
-export const baseURL = `${process.env.VUE_APP_BASE_API}:${process.env.VUE_APP_BASE_PORT}`;
+export const baseURL = `${process.env.VUE_APP_BASE_API}${
+  !isProduction ? `:${process.env.VUE_APP_BASE_PORT}` : ""
+}`;
 
 // socket.io
-export const socketURL = `${process.env.VUE_APP_BASE_API}:${process.env.VUE_APP_SOCKET_PORT}`;
+export const socketURL = `${process.env.VUE_APP_BASE_API}${
+  !isProduction ? `:${process.env.VUE_APP_SOCKET_PORT}` : ""
+}`;
