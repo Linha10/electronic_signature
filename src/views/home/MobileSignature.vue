c62c18df2e1e35d6e85a7dafcff3e748f43a3c68
bill.lin <bill.lin@staff.nueip.com>
2023-11-21 17:51:32 +0800

    w

--- src/views/home/MobileSignature.vue
+++ src/views/home/MobileSignature.vue
@@ -133,6 +133,7 @@ export default {
               });
               // 顯示成功
               this.emitSignatureState = true;
+              this.$socket.disconnect();
               // 關閉簽名彈窗
               this.handleClose();
             },
